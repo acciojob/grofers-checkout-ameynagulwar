9d00180389e089ba56e4b4c1ab713d6e7b8036ff
Amey <130839577+ameynagulwar@users.noreply.github.com>
2023-06-20 17:31:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
+const table = document.getElementById('myTable')
 
 const getSum = () => {
-//Add your code here
+let prices = document.getElementsByClassName('price');
+let sum = 0;
+for(let i = 0; i < prices.length; i++){
+  sum += parseInt(prices[i].innerHTML); 
+}
+let tr = document.createElement('tr');
+let td = document.createElement('td');
+td.innerText = sum;
+td.setAttribute('id','total-price');
+tr.append(td);
+table.append(tr)
   
 };
 
